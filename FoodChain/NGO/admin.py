0bfa4ae8c5959f903db0604eef35c211bf9996ca
mihir7121 <mihir.nikam1@gmail.com>
2020-10-17 17:30:57 +0530

    initial commit

---
+++ FoodChain/NGO/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Belongs,Cities,otherDetails,foodAvbl,Measurement,TypeOf
+
+admin.site.register(Belongs)
+admin.site.register(Cities)
+admin.site.register(otherDetails)
+admin.site.register(foodAvbl)
+admin.site.register(Measurement)
+admin.site.register(TypeOf)
+
+
